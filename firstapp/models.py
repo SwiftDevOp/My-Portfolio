2a56c2900a0df1af397c32ca3907f28ff93b20dd
SwiftDevOp <adebanjiimmanuel@gmail.com>
2025-04-22 23:52:24 +0100

    Review_Section Model

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Review_Section(models.Model):
+    review = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.TextField(max_length=100)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
