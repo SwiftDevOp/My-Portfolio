6f8dd7b116a17740eb638fc6b0478d2df524f5fe
SwiftDevOp <adebanjiimmanuel@gmail.com>
2025-05-05 07:39:32 +0100

    creating adminsuperuser 4

--- firstapp/utils.py
+++ firstapp/utils.py
@@ -3,9 +3,7 @@ from django.contrib.auth import get_user_model
 def create_admin_user():
     User = get_user_model()
     if not User.objects.filter(username="SwiftDevOps").exists():
-        User.objects.create_superuser(
-            username="SwiftDevOps",
-            email="swift@devops.com",
-            password="qwerty@123"
-        )
-        print("✔ Superuser created.")
+        User.objects.create_superuser("SwiftDevOps", "swiftdevops1@example.com", "qwerty@123")
+        print("Superuser SwiftDevOps created.")
+    else:
+        print("Superuser SwiftDevOps already exists.")
